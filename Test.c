894fb720334c38bb1f6f4ab2335a6228994672ad
seunginp <junmo412@gmail.com>
2024-01-20 11:27:33 +0900

    Test Program
    test.c file for practice

---
+++ Test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+void printarr(int parr[]) {
+	for (int i = 0; i < 4; i++) {
+		printf("%d ", &parr[i]);
+		printf("%d ", parr[i]);
+		parr[i]++;
+	}
+}
+
+int main() {
+
+	int arr[4] = { 1,2,3,4 };
+
+	printarr(arr);
+
+	for (int i = 0; i < 4; i++) {
+		printf("%d ", *(arr + i));
+	}
+
+	return 0;
+}
